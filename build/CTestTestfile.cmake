3ad6879f970195344f0cb9174060622698b2951c
Ubuntu16 <daniel.loche@etu.enseeiht.fr>
2017-01-31 04:59:32 -0800

    Workspace ROS

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/danieloche/Documents/ROS_code/src
+# Build directory: /home/danieloche/Documents/ROS_code/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(sensors)
