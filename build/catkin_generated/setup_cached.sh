53de3229f955f791380caf1ff19458bb938bb909
Ubuntu16 <daniel.loche@etu.enseeiht.fr>
2017-02-02 15:25:16 +0100

    gazebo_ros control du robot rrbot

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/danieloche/Documents/ROS_code/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/danieloche/Documents/ROS_code/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/danieloche/Documents/ROS_code/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/danieloche/Documents/ROS_code/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/danieloche/Documents/ROS_code/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/danieloche/Documents/ROS_code/src:$ROS_PACKAGE_PATH"
