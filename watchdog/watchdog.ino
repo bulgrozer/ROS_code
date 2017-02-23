be5d75263e8559fe76c0b42c146af0d48e09c170
Sarah Amar <samar@grenache.laas.fr>
2017-02-23 09:40:42 +0100

    [watchdog]

--- watchdog/watchdog.ino
+++ watchdog/watchdog.ino
@@ -2,79 +2,78 @@
 #include <std_msgs/Bool.h>
 #include <Arduino.h>
 
-class watchdog
+
+ros::NodeHandle ns1;
+ros::NodeHandle ns2;
+ros::NodeHandle np1;
+ros::NodeHandle np2;
+
+std_msgs::Bool raspyState1;
+std_msgs::Bool raspyState2;
+
+
+ros::Publisher pub1("/Raspy1/redundancyMode_topic", &raspyState1);
+ros::Publisher pub2("/Raspy2/redundancyMode_topic", &raspyState2);
+
+
+void callBack1(const std_msgs::Bool& raspyState1)
 {
-	public:
-	watchdog()
-		{
-			// SUBSCRIBERS
-			ros::Subscriber<std_msgs::Bool> sub1("R1_redundancyMode_topic", &callBack1 );
-                        ros::Subscriber<std_msgs::Bool> sub2("R2_redundancyMode_topic", &callBack2 );
-                        
-			// PUBLISHERS
-			ros::Publisher pub1("R1_redundancyMode_topic", &raspyState1);
-                        ros::Publisher pub2("R2_redundancyMode_topic", &raspyState2);
-		}
+  if (raspyState1.data == false )
+  {
+    raspyState2.data = true;
+    pub2.publish( &raspyState2 );
+    np2.spinOnce();
+    delay(1);
+  }
+}
 
-	private:
-        ros::NodeHandle ns1;
-        ros::NodeHandle np1;
-        ros::NodeHandle ns2;
-        ros::NodeHandle np2;
-        
-        std_msgs::Bool raspyState1 ;
-        std_msgs::Bool raspyState2 ;
-	
-	// %Tag(CALLBACK)%
-	void callBack1( const std_msgs::Bool& raspyState1)
-        {
-          if (raspyState1.data == false)
-          {
-            raspyState2.data = true;
-            pub2.publish( &raspyState2 );
-            np2.spinOnce();
-            delay(1000);
-          }
-        }
+void callBack2(const std_msgs::Bool& raspyState2)
+{
+ if (raspyState2.data == false )
+  {
+    raspyState1.data = true;
+    pub1.publish( &raspyState1 );
+    np1.spinOnce();
+    delay(1); 
+  }
+}
 
-        // %Tag(CALLBACK)%
-	void callBack2( const std_msgs::Bool& raspyState2)
-        {
-          if (raspyState2.data == false)
-          {
-            raspyState1.data = true;
-            pub1.publish( &raspyState1 );
-            np1.spinOnce();
-            delay(1000);
-           }
-        }   
-};        
+ros::Subscriber<std_msgs::Bool> sub1("/Raspy1/redundancyMode_topic", &callBack1 );
+ros::Subscriber<std_msgs::Bool> sub2("/Raspy2/redundancyMode_topic", &callBack2 );
 
 
 void setup() {
   // put your setup code here, to run once:
+    
+  ns1.initNode();
+  ns1.subscribe(sub1);
+
+  ns2.initNode();
+  ns2.subscribe(sub2);
+
   np1.initNode();
   np1.advertise(pub1);
-  
-  ns1.initNode();
-  ns1.subscribe(sub);
+  raspyState1.data = true;
+  pub1.publish( &raspyState1 );
+  np1.spinOnce();
+  delay(1); 
   
   np2.initNode();
   np2.advertise(pub2);
-  
-  ns2.initNode();
-  ns2.subscribe(sub);
+  raspyState2.data = false;
+  pub2.publish( &raspyState2 );
+  np2.spinOnce();
+  delay(1);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   
-  watchdog WATCHDOG;
-  
   ns1.spinOnce();
   delay(1);
-  
+
   ns2.spinOnce();
   delay(1);
-  
-}
+
+
+ }
